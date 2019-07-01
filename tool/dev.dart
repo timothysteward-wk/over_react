d10862bb24afd696a7acbc87281e86654d86397d
evanweible-wf <evan.weible@workiva.com>
2019-07-01 12:07:15 -0600

    WIP: ddev v3

--- tool/dev.dart
+++ tool/dev.dart
@@ -12,32 +12,76 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-library tool.dev;
+import 'package:args/command_runner.dart';
+import 'package:dart_dev/commands/sequence_command.dart';
+import 'package:dart_dev/commands/test_command.dart';
+import 'package:dart_dev/configs/workiva.dart' as workiva_ddev_config;
 
-import 'package:dart_dev/dart_dev.dart' show dev, config;
+Iterable<Command<int>> get config => [
+  ...workiva_ddev_config.build(
+    testConfig: TestConfig(
+      commandName: 'test_vm',
+      description: 'Run VM tests.',
+      testArgs: ['-P', 'vm'],
+    ),
+  ),
 
-main(List<String> args) async {
-  const directories = const <String>[
-    'lib/',
-    'test/',
-    'tool/',
-  ];
+  TestCommand(
+    TestConfig(
+      commandName: 'test_ddc',
+      description: 'Run browser tests via DDC.',
+      testArgs: ['-P', 'dartdevc'],
+    ),
+  ),
 
-  config.analyze.entryPoints = directories;
-  config.copyLicense.directories = ['integrate']..addAll(directories);
+  TestCommand(
+    TestConfig(
+      commandName: 'test_dart2js',
+      description: 'Run browser tests via dart2js.',
+      buildRunnerArgs: ['-r'],
+      testArgs: ['-P', 'dart2js']
+    ),
+  ),
 
-  config.test
-    ..platforms = [
-      'vm',
-      'chrome',
-    ]
-    ..unitTests = [
-      'test/vm_tests/',
-      'test/over_react_component_declaration_test.dart',
-      'test/over_react_component_test.dart',
-      'test/over_react_dom_test.dart',
-      'test/over_react_util_test.dart',
-    ];
+  SequenceCommand(
+    SequenceConfig(
+      commandName: 'test',
+      description: 'Run all tests in this package.',
+      primaryCommands: [
+        ['test_vm'],
+        ['test_ddc'],
+        ['test_dart2js'],
+      ],
+    ),
+  ),
+];
 
-  await dev(args);
-}
+// library tool.dev;
+
+// import 'package:dart_dev/dart_dev.dart' show dev, config;
+
+// main(List<String> args) async {
+//   const directories = const <String>[
+//     'lib/',
+//     'test/',
+//     'tool/',
+//   ];
+
+//   config.analyze.entryPoints = directories;
+//   config.copyLicense.directories = ['integrate']..addAll(directories);
+
+//   config.test
+//     ..platforms = [
+//       'vm',
+//       'chrome',
+//     ]
+//     ..unitTests = [
+//       'test/vm_tests/',
+//       'test/over_react_component_declaration_test.dart',
+//       'test/over_react_component_test.dart',
+//       'test/over_react_dom_test.dart',
+//       'test/over_react_util_test.dart',
+//     ];
+
+//   await dev(args);
+// }
